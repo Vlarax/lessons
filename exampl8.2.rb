613f20dca78d0d335c0860de33d0957547372b63
Vlarax <93268652+Vlarax@users.noreply.github.com>
2021-10-28 16:56:53 +0300

    Add files via upload

---
+++ exampl8.2.rb
@@ -0,0 +1,10 @@
+names = ["dad", "cat", "dog", "log"] # массив
+
+
+names.each do |en| #каждый элемент массива перебираем и записываем в переменныю en
+puts en + "!"
+end
+6.times do |index| #.times 6 раз выполняется итерация в index
+  puts index
+
+end
